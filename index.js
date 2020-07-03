4d8575e88c9e1ce849e56cdee3963d4122f8a117
Nadia Jamhari <62807626+nadiajamhari@users.noreply.github.com>
2020-07-03 14:59:38 +0800

    Create index.js
    Adding index.js for navigation

---
+++ index.js
@@ -0,0 +1,10 @@
+ $(window).scroll(function(){
+    $('nav').toggleClass('scrolled',$(this).scrollTop()>585)
+  
+ });
+
+ $(window).scroll(function(){
+     $('#register').toggleClass('scrolled',$(this).scrollTop()>585)
+ });
+
+
